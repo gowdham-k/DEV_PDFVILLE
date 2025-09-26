dd44441b87332d7063321911c09459dab8ec8d64
KHEKHO\selva <selvarajankishore@gmail.com>
2025-09-26 10:59:40 +0530

    missing hubspot file

---
+++ frontend/components/HubspotTracking.js
@@ -0,0 +1,49 @@
+import Script from 'next/script';
+import { useEffect } from 'react';
+
+const HUBSPOT_PORTAL_ID = '243665895';
+
+/**
+ * HubspotTracking component for adding HubSpot tracking to pages
+ * @param {Object} props - Component props
+ * @param {string} props.pageName - Name of the current page for tracking
+ * @param {Object} props.pageData - Additional data to track with the page view
+ */
+const HubspotTracking = ({ pageName, pageData = {} }) => {
+  // Track page view when component mounts
+  useEffect(() => {
+    if (window.hubspot) {
+      window.hubspot.track(pageName || 'Page Viewed', pageData);
+    }
+  }, [pageName, pageData]);
+
+  return (
+    <Script
+      id="hubspot-tracking"
+      strategy="afterInteractive"
+      dangerouslySetInnerHTML={{
+        __html: `
+          (function(d,s,i,r) {
+            if (d.getElementById(i)){return;}
+            var n=d.createElement(s),e=d.getElementsByTagName(s)[0];
+            n.id=i;n.src='//js.hs-scripts.com/${HUBSPOT_PORTAL_ID}.js';
+            e.parentNode.insertBefore(n, e);
+          })(document,"script","hs-script-loader");
+        `
+      }}
+    />
+  );
+};
+
+/**
+ * Track an event in HubSpot
+ * @param {string} eventName - Name of the event to track
+ * @param {Object} eventData - Additional data to track with the event
+ */
+export const trackEvent = (eventName, eventData = {}) => {
+  if (typeof window !== 'undefined' && window.hubspot) {
+    window.hubspot.track(eventName, eventData);
+  }
+};
+
+export default HubspotTracking;
